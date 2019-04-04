06210d7d163678a2f42788f08830cd144813740b
Shubham Raichandel <shubhamraichandel@yahoo.com>
2019-04-04 01:20:30 -0400

    added contact list

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -12,7 +12,7 @@ export class AuthService {
   private authToken: any;
 
   private endpoint = 'https://comp308-assgn2.herokuapp.com/api/';
-  //private endpoint = 'http://localhost:3000/api/';
+  // private endpoint = 'http://localhost:3000/api/';
 
   private httpOptions = {
     headers: new HttpHeaders({
